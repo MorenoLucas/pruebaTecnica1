db1f8e0efbac101e900c7e45ece2888da4c9077e
MorenoLucas <sl.lucasmoreno@gmail.com>
2021-06-09 15:18:55 +0200

    creamos interfaces

--- src/app/services/servicios.service.ts
+++ src/app/services/servicios.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ServiciosService {
-
-  constructor() { }
+  productos;
+  constructor() {}
+  getProductos() {
+    return this.productos;
+  }
+  setProductos(producto) {
+    this.productos = [...producto];
+  }
+  agregarProducto(producto) {
+    const id = this.productos.findIndex((productoParam) => {
+      return this.productos.id === productoParam.id;
+    });
+    if (id >= 0) {
+      this.productos[id].cantidad += producto.cantidad;
+    } else {
+      this.productos.push(producto);
+    }
+  }
 }
